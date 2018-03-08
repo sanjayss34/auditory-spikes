21f9e9a442d9cdf0d65f58505af610c4f82550c8
christopher-lo <invinciblechicken@gmail.com>
2018-03-08 16:19:38 -0500

    Vectorized code

--- estimate_ising.m
+++ estimate_ising.m
@@ -1,41 +1,97 @@
-function [h0 J] = estimate_ising()
-    load('neuron_trains.mat');
+function [h0 J] = estimate_ising(iters)
+    load 'neuron_trains.mat' neuron_trains;
     neuron_trains = cell2mat(neuron_trains);
     [N T] = size(neuron_trains);
-    h0 = unifrnd(0, 1, 1, N);
-    J = unifrnd(0, 1, N, N);
+    h0 = unifrnd(-1, 1, 1, N);
+    J = unifrnd(-1, 1, N, N);
     maxdiff = 1;
-    eta = 1;
+    eta = 0.1;
     alpha = 0;
     prev_change_h0 = zeros(1, N);
     prev_change_J = zeros(N, N);
-    itercount = 1;
-    while itercount < 50
+    itercount = 0;
+    
+    % Measure deviations from experiment
+    sigma_diff = zeros(1,iters);
+    corr_diff = zeros(1,iters);
+    
+    % Gradient Ascent
+    mean_experiment = transpose(mean(neuron_trains,2));
+    mean_experiment_product = neuron_trains*transpose(neuron_trains)/size(neuron_trains,2);
+    while itercount < iters
+        
+        tic;
         % eta = eta/itercount;
-        disp([itercount maxdiff]);
         itercount = itercount+1;
+        disp([itercount maxdiff]);
         maxdiff = 0;
-        sample_size = 100;
-        samples = sample_ising(sample_size, h0, J);
-        for i=1:N
-            mean_sigma_i = mean(samples(:,i));
-            mean_experiment_i = mean(neuron_trains(i,:));
-            diff = eta*(mean_experiment_i-mean_sigma_i)+alpha*prev_change_h0(i);
-            maxdiff = max(maxdiff, abs(diff));
-            prev_change_h0(i) = diff;
-            h0(i) = h0(i)+diff;
-        end
-        for i=1:N
-            for j=i+1:N
-                mean_experiment_product = mean(neuron_trains(i,:).*neuron_trains(j,:));
-                mean_product = mean(samples(:,i).*samples(:,j));
-                diff = eta*(mean_experiment_product-mean_product)+alpha*prev_change_J(i,j);
-                maxdiff = max(maxdiff, abs(diff));
-                prev_change_J(i, j) = diff;
-                prev_change_J(j, i) = diff;
-                J(i, j) = J(i,j)+diff;
-                J(j, i) = J(j,i)+diff;
-            end
+        
+        % Sample Ising Estimations
+        sample_size = 500;
+        %samples = sample_ising(sample_size, h0, J);
+        samples = mh_sample_ising(1, sample_size, h0, J, 500);
+        toc
+        
+        tic;
+        % Update h0 
+        mean_sigma = mean(samples);
+        diff = eta*(mean_experiment-mean_sigma) + alpha*prev_change_h0;
+        prev_change_h0 = diff;
+        h0 = h0 + diff;
+        sigma_diff(itercount) = mean(abs(mean_experiment-mean_sigma));
+        toc
+        
+        tic;
+        % Update Jij
+        mean_product = transpose(samples)*samples/size(samples,1);
+        diff = eta*(mean_experiment_product-mean_product)+alpha*prev_change_J;
+        diff(logical(eye(size(diff)))) = 0;
+        maxdiff = max(max(max(maxdiff, abs(diff))));
+        prev_change_J = diff;
+        J = J + diff;
+        corr_diff(itercount) = sum(sum(abs(mean_experiment_product-mean_product)))/(N^2);
+        toc
+    end
+    
+    % Plot deviation from experiment over time
+    figure;
+    subplot(2,1,1);
+    plot(1:iters, sigma_diff);
+    title('Deviation of Mean Firing Rate');
+    subplot(2,1,2);
+    plot(1:iters, corr_diff);
+    xlabel('# of Iterations');
+    title('Deviation of Mean Correlation');
+    
+    % Mean responses
+    mrs = mean_sigma;
+    mers = mean_experiment;
+    % Mean products
+    num_entries = N*(N-1)/2;
+    meps = zeros(1,num_entries);
+    mps = zeros(1,num_entries);
+    k = 1;
+    for i = 1:N
+        for j = i+1:N
+            meps(k) = mean_experiment_product(i,j);
+            mps(k) = mean_product(i,j);
+            k = k+1;
         end
     end
+    
+    % Plot predicted vs. empirical values
+    figure;
+    scatter(mers, mrs, 10, 'b', 'filled');
+    hold on;
+    xlabel('Mean Experimental Response');
+    ylabel('Mean Predicted Response');
+    lin = linspace(min(min(mers),min(mrs)),max(max(mers),max(mrs)),101);
+    plot(lin, lin, 'r')
+    figure;
+    scatter(meps, mps, 10, 'b', 'filled');
+    hold on;
+    xlabel('Mean Experimental Correlation');
+    ylabel('Mean Predicted Correlation');
+    lin = linspace(min(min(meps),min(mps)),max(max(meps),max(mps)),101);
+    plot(lin, lin, 'r')
 end
--- mh_sample_ising.m
+++ mh_sample_ising.m
@@ -1,4 +1,4 @@
-function [samples]  = mh_sample_ising(beta, sigm0, m, h0, J, iters)  
+function [samples] = mh_sample_ising(beta, m, h0, J, iters, sigm0)  
     N = numel(h0);
     samples = zeros(m, N);    
     for i = 1:m
@@ -10,13 +10,13 @@ function [samples]  = mh_sample_ising(beta, sigm0, m, h0, J, iters)
         selected_neurons = randi([1 N], 1, iters);
         for t = 1:iters
             % Current energy
-            exponential_arg = sum(h0.*sigm)+0.5*sum(sum(J.*(transpose(sigm)*sigm)));
-            Hu = exp(exponential_arg);
+            exponential_arg_u = -1*(sum(h0.*sigm)+0.5*sum(sum(J.*(transpose(sigm)*sigm))));
+            Hu = exponential_arg_u;
             % Flipped energy
             temp_sigm = sigm;
             temp_sigm(selected_neurons(t)) = -temp_sigm(selected_neurons(t));
-            exponential_arg = sum(h0.*temp_sigm)+0.5*sum(sum(J.*(transpose(temp_sigm)*temp_sigm)));
-            Hv = exp(exponential_arg);
+            exponential_arg_v = -1*(sum(h0.*temp_sigm)+0.5*sum(sum(J.*(transpose(temp_sigm)*temp_sigm))));
+            Hv = exponential_arg_v;
             % Compare energies
             if (Hv-Hu) > 0
                 A = exp(-beta*(Hv-Hu));
