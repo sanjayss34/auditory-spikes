da9b5144d186d6bebe946f0e821d22c84e4b45db
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2018-04-19 14:08:47 -0400

    sta is better?

--- sta.m
+++ sta.m
@@ -3,7 +3,7 @@ load('CohenNeurons.mat');
 load('rawStimuli.mat');
 trials_stimulus = rawStimCollector;
 N = 16;
-D = zeros(N, 100);
+D = zeros(N, 5000);
 load('aud_stream.mat');
 fs = Audstim.fs/1000;
 for n=1:N
@@ -15,13 +15,15 @@ for i=1:numel(CohenNeurons(n).trials)
     for t=1:numel(spikes)
         for j=1:min(size(D, 2), spikes(t)-1)
             if spikes(t) > 0 && spikes(t) <= 2000
-                contributions(j) = contributions(j)+stimulus(cast((spikes(t)-j)*fs, 'int8'));
+                contributions(j) = contributions(j)+stimulus(cast((spikes(t))*fs, 'int32')-j);
                 count_spikes = count_spikes+1;
             end
         end
     end
-    contributions = contributions/count_spikes;
-    D(n,:) = D(n,:)+contributions;
+    if count_spikes > 0
+        contributions = contributions/count_spikes;
+        D(n,:) = D(n,:)+contributions;
+    end
 end
 D(n,:) = D(n,:)/numel(CohenNeurons(n).trials);
 end
